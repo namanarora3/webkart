5cb6bc6b7b9feed64e7bd4ad3585a2eeb51e4e25
namanarora3 <naman30103@gmail.com>
2023-08-28 03:56:15 +0530

    Added Custom user model

--- webkart/core/models.py
+++ webkart/core/models.py
@@ -1,5 +1,42 @@
 from django.db import models
 
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
 
 
+class UserManager(BaseUserManager):
 
+    def create_user(self, email, password=None, **other_fields):
+        if not email:
+            raise(ValueError("Email not provided"))
+        user = self.model(
+            email=self.normalize_email(email),
+            **other_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        # map = {'number' : 123}
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    number = models.IntegerField(unique=True, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_seller = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
