985d2fd0bf512e48b34245ad04baed3edf7e7c23
namanarora3 <naman30103@gmail.com>
2023-08-30 01:47:24 +0530

    added user category with token

--- webkart/user/views.py
+++ webkart/user/views.py
@@ -9,6 +9,10 @@ from rest_framework.authtoken.views import ObtainAuthToken
 
 from django.shortcuts import redirect
 
+from rest_framework.response import Response
+
+from rest_framework.authtoken.models import Token
+
 
 class CreateUserView(generics.CreateAPIView):
     '''NO AUTH HEADER REQD. Create a new user in the system.'''
@@ -30,7 +34,13 @@ class CreateTokenView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('http://127.0.0.1:8000/api/user/my-profile')
-        return super().post(request, *args, **kwargs)
+        # from super function
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        is_seller = getattr(user,'is_seller')
+        return Response({'token': token.key, 'is_seller': is_seller})
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     '''AUTH HEADER REQD. View and Update details for logged in user'''
